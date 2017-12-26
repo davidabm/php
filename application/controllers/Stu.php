b39df90238c0a56cb31db71cbad293acad90b335
david <davidabm@163.com>
2017-12-26 16:58:26 +0800

    1

--- application/controllers/Stu.php
+++ application/controllers/Stu.php
@@ -10,15 +10,10 @@ class News extends CI_Controller {
 
     public function index()
     {
-        $data['news'] = $this->news_model->get_news();
         $data['title'] = 'News archive';
 
         $this->load->view('templates/header', $data);
-        $this->load->view('news/index', $data);
+        $this->load->view('stu/index', $data);
         $this->load->view('templates/footer');
-    }
-    public function view($slug = NULL)
-    {
-        $data['news_item'] = $this->news_model->get_news($slug);
     }
 }
