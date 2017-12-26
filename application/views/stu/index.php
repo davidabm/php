78fc995fd09c5ca8d61a3bbd5bb5e7e356de2a63
david <davidabm@163.com>
2017-12-26 17:16:02 +0800

    1

--- application/views/stu/index.php
+++ application/views/stu/index.php
@@ -1,17 +1,24 @@
-
-<div class="weui-cells__title">姓名</div>
-<div class="weui-cells">
-    <div class="weui-cell">
-        <div class="weui-cell__bd">
-            <input class="weui-input" type="text" placeholder="请输入学号"/>
-        </div>
+<div class="page">
+	<div class="page__hd">
+        <h1 class="page__title">
+            <img src="./images/logo.png" alt="WeUI" height="21px" />
+        </h1>
+        <p class="page__desc">WeUI 是一套同微信原生视觉体验一致的基础样式库，由微信官方设计团队为微信内网页和微信小程序量身设计，令用户的使用感知更加统一。</p>
     </div>
-    <div class="weui-cell">
-        <div class="weui-cell__bd">
-            <input class="weui-input" type="text" placeholder="请输入姓名"/>
-        </div>
-    </div>
-</div>
-<div class="weui-btn-area">
-    <a class="weui-btn weui-btn_primary" href="javascript:" id="showTooltips">确定</a>
+	<div class="weui-cells__title">姓名</div>
+	<div class="weui-cells">
+	    <div class="weui-cell">
+	        <div class="weui-cell__bd">
+	            <input class="weui-input" type="text" placeholder="请输入学号"/>
+	        </div>
+	    </div>
+	    <div class="weui-cell">
+	        <div class="weui-cell__bd">
+	            <input class="weui-input" type="text" placeholder="请输入姓名"/>
+	        </div>
+	    </div>
+	</div>
+	<div class="weui-btn-area">
+	    <a class="weui-btn weui-btn_primary" href="javascript:" id="showTooltips">确定</a>
+	</div>
 </div>
