5ebebb54cc084509a82bcd72794defd061271df8
yuwei <davidabm@163.com>
2017-12-27 22:15:50 +0800

    1

--- application/models/Stu_model.php
+++ application/models/Stu_model.php
@@ -6,9 +6,9 @@ class Stu_model extends CI_Model {
     }
     public function search_stu($stu)
 	{
-	    $this -> db -> where('sty', $name);
+	    $this -> db -> where('name', $name);
 	    $this -> db -> select('*'); //选取全部信息
-	    $query = $this -> db -> get('name');
+	    $query = $this -> db -> get('sty');
 	    return $query -> result(); //返回值
 	}
 
