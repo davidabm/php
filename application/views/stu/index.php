8da4ce24e99c5d39c7c1ba1e858426f897932c41
david <davidabm@163.com>
2017-12-27 10:48:29 +0800

    1

--- application/views/stu/index.php
+++ application/views/stu/index.php
@@ -1,10 +1,14 @@
+<style>	
+	.page__hd {
+    padding: 0 15px;
+}
+</style>
 <div class="page">
 	<div class="page__hd">
         <h1 class="page__title">
             <img src="./images/logo.png" alt="WeUI" height="21px" />
         </h1>
         <p class="page__desc">WeUI 是一套同微信原生视觉体验一致的基础样式库，由微信官方设计团队为微信内网页和微信小程序量身设计，令用户的使用感知更加统一。</p>
-    </div>
 	<div class="weui-cells__title">姓名</div>
 	<div class="weui-cells">
 	    <div class="weui-cell">
@@ -21,4 +25,5 @@
 	<div class="weui-btn-area">
 	    <a class="weui-btn weui-btn_primary" href="javascript:" id="showTooltips">确定</a>
 	</div>
+    </div>
 </div>
