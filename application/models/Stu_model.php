1c336a061d9e1ee0cf5445992a9f7f158fb5da22
yuwei <davidabm@163.com>
2017-12-30 08:16:42 +0800

    1

--- application/models/Stu_model.php
+++ application/models/Stu_model.php
@@ -4,7 +4,13 @@ class Stu_model extends CI_Model {
     {
         $this->load->database();
     }
-    
+    public function search_stu($stu)
+	{
+	    $this -> db -> where('name', $stu);
+	    $this -> db -> select('*'); //选取全部信息
+	    $query = $this -> db -> get('sty');
+	    return $query -> result(); //返回值
+	}
 	public function u_update() {
 
 	    $this->load->helper('url');
