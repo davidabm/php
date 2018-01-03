526e55a4dcd3c755d2f0d17af608f848ade95129
yuwei <davidabm@163.com>
2018-01-03 17:25:10 +0800

    3

--- application/controllers/Stu.php
+++ application/controllers/Stu.php
@@ -39,7 +39,7 @@ class Stu extends CI_Controller {
 
                 $this -> stu_model -> u_update();
 
-                $data['stu_item'] = $this->stu_model->get_stu($name);
+                $data['stu_item'] = $this->stu_model->get_stu($number);
 
                 $this->load->view('templates/header');
                 $this->load->view('stu/show', $data);
